7882489f983cf78214d1b549ef1143c72e659442
imvaibhav99 <imvaibhav9796@gmail.com>
2025-03-01 02:57:25 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@
 const express= require("express");
 const app=express();
 //server is running atr localhost:7777
-app.use("/",(req,res)=>{
-    res.send("Hello from the server!")
-})
 
 //Giving routes along with req,res->localhost:7777/test
 //Server will run at 7777 but o/p at /test
-app.use("/test",(req,res)=>{
-    res.send("test Route ")
+
+//get handles the GET api call 
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Vaibhav",
+        lastname:"Saini"
+    });
 })
+ 
+//post handles POST api calls
+app.post("/user",(req,res)=>{
+    res.send("test get call");
+})
+
+//ues handles all api calls
+app.use("/test",(req,res)=>{
+    res.send("tested successfully");
+})
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the server!")
+// })
+
 app.listen(7777,()=>{
     console.log("Server is listening at port 7777");
     
