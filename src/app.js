7d98434483ddf48dd79aa5d7819f959fd04e9255
imvaibhav99 <imvaibhav9796@gmail.com>
2025-03-01 14:18:28 +0530

    middleware and auth

--- src/app.js
+++ src/app.js
@@ -1,39 +1,16 @@
 //creating a new server using express->npm i express
 const express= require("express");
 const app=express();
-//server is running atr localhost:7777
-
-//Giving routes along with req,res->localhost:7777/test
-//Server will run at 7777 but o/p at /test
-
-//get handles the GET api call 
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Vaibhav",
-        lastname:"Saini"
-    });
+// //server is running at localhost:7777
+const {adminAuth}= require("./middlewares/auth");
+//Handle Auth Middleware for all GET POST ..... requests->always use app.use
+app.use("/admin",adminAuth)
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All data sent");
 })
- 
-//post handles POST api calls
-// app.post("/user",(req,res)=>{
-//     // res.send("test get call");
-   
-// })
-app.post("/user",(req,res,next)=>{
-    //  res.send("test get call");
-    next();
-   // res.send("test get call");
-},(req,res)=>{
-    res.send("second response");
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Delete a user");
 })
-
-//ues handles all api calls
-app.use("/test",(req,res)=>{
-    res.send("tested successfully");
-})
-// app.use("/",(req,res)=>{
-//     res.send("Hello from the server!")
-// })
-
 app.listen(7777,()=>{
     console.log("Server is listening at port 7777");
     
---
+++ src/middlewares/auth.js
@@ -0,0 +1,13 @@
+ const adminAuth=(req,res,next)=>{
+    console.log("Admin auth is getting checked");
+    const token="xyz";
+    const isAdminAuthorised=token==="xyz";
+    if(!isAdminAuthorised){
+        res.status(401).send("Unauthorised request")
+    }else{
+        next();
+    }
+}
+module.exports={
+    adminAuth,
+};
